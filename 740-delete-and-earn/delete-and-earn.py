bd4ff46c4e641a444210baa2f300c740b82fb150
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-07-21 15:43:59 +0100

    Time: 10 ms (50.87%) | Memory: 20.2 MB (25.52%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        counts = sorted([(k, v) for k, v in Counter(nums).items()])
+        scores = [0 for _ in counts]
+        for i, item in enumerate(counts):
+            num, count = item
+            if i >= 2 and counts[i-1][0] == num - 1:
+                scores[i] = max(num * count, num * count + scores[i-2], scores[i-1])
+            elif i >= 1 and counts[i-1][0] < num - 1:
+                scores[i] = num * count + scores[i-1]
+            elif i >= 1:
+                scores[i] = max(num * count, scores[i-1])
+            else:
+                scores[i] = num * count
+        print(counts)
+        print(scores)
+        return max(scores)
+            
