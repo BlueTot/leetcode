bf17d345ff09fb5fd8dac117a63fde0e87fb86dd
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-07-23 09:09:53 +0100

    Time: 913 ms (5.51%) | Memory: 18.3 MB (26.36%) - LeetSync

---
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        lts = [(0, 0) for _ in nums]
+        best = 0
+        for i in range(len(nums)):
+            if i == 0:
+                lts[i] = (1, 1)
+            else:
+                largest = 0
+                for j in range(i):
+                    if nums[j] < nums[i]: 
+                        largest = max(largest, lts[j][0])
+                count = 0
+                for j in range(i):
+                    if nums[j] < nums[i] and lts[j][0] == largest:
+                        count += lts[j][1]
+                lts[i] = (largest + 1, max(1, count))
+            best = max(best, lts[i][0])
+        print(lts, best)
+        output = 0
+        for length, count in lts:
+            if length == best:
+                output += count
+        return output
+        
