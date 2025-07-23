ad79915ab1262e12d8d7184aabda27332cddb0ac
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-07-23 23:08:54 +0100

    Time: 482 ms (5.02%) | Memory: 42.3 MB (5.18%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.py
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.py
@@ -8,19 +8,11 @@ class Solution:
                 indices[num] = set()
             indices[num].add(i)
 
-        # indices = {v: k for k, v in enumerate(arr)}
-
         for i in range(len(arr)):
             longest = 0
-            # for j in range(i):
             if (k := arr[i] - difference) in indices:
                 for j in indices[k]:
                     longest = max(longest, las[j])
-            # if (k := arr[i] - difference) in indices:
-            #     longest = max(longest, las[indices[k]])
-                # if arr[i] - arr[j] == difference:
-                #     longest = max(longest, las[j])
-                # print(i, k, las[k], longest)
             las[i] = 1 + longest
         print(las, indices)
         return max(las)
