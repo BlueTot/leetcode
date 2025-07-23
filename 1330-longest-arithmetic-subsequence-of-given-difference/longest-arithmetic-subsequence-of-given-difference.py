0ccc05df10ea32ff7fae159d957c5de0ab23389a
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-07-23 23:09:43 +0100

    Time: 403 ms (5.02%) | Memory: 39.7 MB (5.18%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.py
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.py
@@ -2,6 +2,7 @@ class Solution:
     def longestSubsequence(self, arr: List[int], difference: int) -> int:
         las = [0 for _ in arr]
 
+        # organise values against their indices
         indices = {}
         for i, num in enumerate(arr):
             if num not in indices:
@@ -10,9 +11,10 @@ class Solution:
 
         for i in range(len(arr)):
             longest = 0
+            # only check values that are valid
             if (k := arr[i] - difference) in indices:
                 for j in indices[k]:
                     longest = max(longest, las[j])
             las[i] = 1 + longest
-        print(las, indices)
+
         return max(las)
