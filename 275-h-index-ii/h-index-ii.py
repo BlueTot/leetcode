67c3015d3ee85fa5454db90f70a3460a7739e07f
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-08-07 21:57:53 +0100

    Time: 0 ms (100.00%) | Memory: 23 MB (35.64%) - LeetSync

--- 275-h-index-ii/h-index-ii.py
+++ 275-h-index-ii/h-index-ii.py
@@ -1,30 +1,20 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        print([len(citations) - i < citations[i] for i in range(len(citations))])
-
-
-
-
 
         left = -1
         right = len(citations)
-        # if left == right:
-        #     return min(citations[left], 1)
+
+        # template to find minimum index that satisfies a condition
         while left < right:
             mid = (left + right) // 2
-            print(left, right, mid)
+
+            # negate the condition to use template
             if len(citations) - mid < citations[mid]:
                 right = mid
             else:
                 left = mid + 1
-        print(left, right)
+
+        # at best we can have len(citations)-left as the H index
         if left > 0:
             return max(len(citations)-left, citations[left-1])
-        # if left > 0:
-        #     return citations[left-1] if citations[left-1] > 0 else len(citations)-left
         return len(citations)
-        # print(left-1)
-        # return citations[left]
-        # if left-1 == -1: # out of bounds
-        #     return min(citations[0], 1)
-        # return citations[left-1]
