3ec3b2e34a56f30644b4e31f5322d672eef6d3fe
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-08-11 22:28:08 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (82.23%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -6,19 +6,17 @@ class Solution:
         curr = ""
         while n > 0:
             
+            # calculate first digit by subtracting number of ways
+            # to arrange remaining digits
             amount = 0
             fac = factorial(n-1)
             while k > fac:
                 k -= fac
                 amount += 1
 
+            # add to curr, and remove that digit
             curr += str(nums[amount])
             nums.pop(amount)
             n -= 1
 
         return curr
-        # 3! = 6. 9 // 6 = 1, so we use second position - 2
-        # 9 - 6 = 3 [1,3,4]
-        # 2! = 2. 3 // 2 = 1, so we use second position - 3
-        # 3 - 2 = 1 [1,4]
-        # 1! = 1. 1 // 1 = 1
