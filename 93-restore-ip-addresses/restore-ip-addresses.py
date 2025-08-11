147b41ba8daa2194c5c108967b20c93711e9f634
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-08-11 22:43:59 +0100

    Time: 47 ms (5.07%) | Memory: 18 MB (28.89%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+
+        addresses = set()
+        def populate(s: str, curr: List[str]) -> List[str]:
+
+            if not s and len(curr) == 4:
+                addresses.add(".".join(curr))
+                return
+            if not s:
+                return
+
+            # print(s, curr)
+            for i in range(1, 4):
+                # in range, and has no leading zeroes
+                if 0 <= int(s[:i]) <= 255 and str(int(s[:i])) == s[:i]:
+                    curr.append(s[:i])
+                    populate(s[i:], curr)
+                    curr.pop()
+
+        
+        populate(s, [])
+        return list(addresses)
