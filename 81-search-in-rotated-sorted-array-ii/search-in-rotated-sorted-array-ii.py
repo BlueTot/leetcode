8c23a318f047899fb2ef859e2982a4fa6876db26
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-08-12 11:58:00 +0100

    Time: 0 ms (100.00%) | Memory: 18.2 MB (79.52%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,8 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         
-        for num in nums:
-            if num == target:
-                return True
-        
-        return False
+        # worst case time complexity is O(n)
+        # consider array [10, 1, 10, 10, 10, 10]
+        # same as a linear search here
+        return target in nums
