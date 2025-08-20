55b8232e1ba492cf4e10c3643974ce48818f6b51
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-08-20 14:21:31 +0100

    Time: 224 ms (23.43%) | Memory: 44.7 MB (5.90%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -12,6 +12,7 @@ class TreeNode:
 class Trie:
     def __init__(self):
         self.__root = TreeNode({}, '', False)
+        self.__threeSmallests = {}
     
     def insert(self, word: str):
         curr = self.__root
@@ -30,19 +31,46 @@ class Trie:
             result.extend(self.__findAll(node.children[char], curr+char))
         return result
     
+    def __findThreeSmallest(self, node: "TreeNode", curr: str):
+        result = []
+        if node.isTerminal:
+            result.extend([curr])
+        for char in sorted(node.children.keys()):
+            smallests = self.__findThreeSmallest(node.children[char], curr+char)
+            if len(result) <= 3:
+                result.extend(smallests)
+        result = result[:min(3, len(result))]
+        self.__threeSmallests[curr] = result
+        return result
+    
+    
     def searchByCharacter(self, searchWord: str) -> List[List[str]]:
-        curr = self.__root
+        self.__findThreeSmallest(self.__root, "")
+        print(self.__threeSmallests)
         result = []
         foundEmpty = False
+        currStr = ""
+        curr = self.__root
         for i, char in enumerate(searchWord):
+            currStr += char
             if foundEmpty or char not in curr.children:
                 result.append([])
                 foundEmpty = True
             else:
-                words = self.__findAll(curr.children[char], searchWord[:i+1])
-                result.append(words if len(words) <= 3 else words[:3])
+                result.append(self.__threeSmallests[currStr])
                 curr = curr.children[char]
         return result
+        # result = []
+        # foundEmpty = False
+        # for i, char in enumerate(searchWord):
+        #     if foundEmpty or char not in curr.children:
+        #         result.append([])
+        #         foundEmpty = True
+        #     else:
+        #         words = self.__findAll(curr.children[char], searchWord[:i+1])
+        #         result.append(words if len(words) <= 3 else words[:3])
+        #         curr = curr.children[char]
+        # return result
     
     def __repr__(self):
         return str(self.__root)
