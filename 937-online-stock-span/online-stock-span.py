6cb02a69b14a85b542ec6721a3b5dcf541e12f27
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-08-20 17:38:38 +0100

    Time: 85 ms (14.70%) | Memory: 22.9 MB (34.87%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -2,16 +2,14 @@ class StockSpanner:
 
     def __init__(self):
         self.__i = 0
-        self.__mstack = []
-        self.__prices = []
+        self.__mstack: List[Tuple[int, int]] = []
         
 
     def next(self, price: int) -> int:
-        self.__prices.append(price)
-        while self.__mstack and self.__prices[self.__mstack[-1]] <= price:
+        while self.__mstack and self.__mstack[-1][0] <= price:
             self.__mstack.pop()
-        result = self.__i - (self.__mstack[-1] if self.__mstack else -1)
-        self.__mstack.append(self.__i)
+        result = self.__i - (self.__mstack[-1][1] if self.__mstack else -1)
+        self.__mstack.append((price, self.__i))
         self.__i += 1
         return result
         
