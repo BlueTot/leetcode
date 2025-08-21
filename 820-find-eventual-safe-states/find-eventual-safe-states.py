8b3c6c3e7696cd70c66295d6101e08e77e1034b3
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-08-21 10:45:04 +0100

    Time: 62 ms (33.99%) | Memory: 24.3 MB (14.10%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -5,23 +5,33 @@ class Solution:
         visited = set()
 
         def dfs(node, stack):
-            # print(node, stack)
+
+            # if back edge (in stack), it is not safe.
             if node in stack:
                 safe[node] = False
                 return
+
+            # if we've seen it before, skip it
             if node in visited:
                 return
+
             visited.add(node)
             stack.add(node)
+
             for neighbour in graph[node]:
+
+                # traverse to neighbour
                 dfs(neighbour, stack)
+
+                # if neighbour isn't safe, current isn't safe
                 if not safe[neighbour]:
                     safe[node] = False
+
             stack.remove(node)
 
         for i in range(len(graph)):
+            # if we haven't visited it, traverse to it
             if i not in visited:
-                # print(i)
                 dfs(i, set()) 
         
         return [i for i in range(len(graph)) if safe[i]]
