5249fdf82116d24c40f992abc049de322c2afde2
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-09-29 14:06:36 +0100

    Time: 0 ms (100%), Space: 11 MB (41.6%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+        vector<pair<TreeNode*, int>> stack;
+        stack.push_back({root, 0});
+        vector<int> result;
+        TreeNode* node;
+
+        while (stack.size() > 0) {
+
+            auto pair = stack.back();
+            if (pair.first == NULL) {
+                stack.pop_back();
+                continue;
+            }
+
+            if (pair.second == 0) {
+                stack.back().second++;
+                result.push_back(pair.first->val);
+                
+            } else if (pair.second == 1) {
+                stack.back().second++;
+                stack.push_back({pair.first->left, 0});
+
+            } else if (pair.second == 2) {
+                stack.back().second++;
+                stack.push_back({pair.first->right, 0});
+                
+            } else {
+                stack.pop_back();
+            }    
+        }
+
+        return result;
+    }
+};
