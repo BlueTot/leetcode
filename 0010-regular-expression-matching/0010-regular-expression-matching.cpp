aff046421a42ceb182f527a6810545e4218f8ff3
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-10-02 18:20:49 +0100

    Time: 3 ms (52.21%), Space: 9.7 MB (18.4%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -20,43 +20,37 @@ public:
         
         vector<string> matches = parsePattern(p);
 
-        // for (int i = 0; i < matches.size(); i++) {
-        //     std::cout << matches[i] << ", ";
-        // }
-        // std::cout << "\n";
-
         vector<vector<bool>> dp(
             s.size() + 1,
             vector<bool>(matches.size() + 1, false)
         );
-        dp[0][0] = true;
+
+        dp[0][0] = true; // empty string, empty match is ok
+
         for (int j = 0; j < matches.size(); j++) {
-            if (matches[j].size() == 1) { // character
-                dp[0][j+1] = false;
-            } else { // star
-                dp[0][j+1] = dp[0][j];
+            if (matches[j].size() == 1) {
+                dp[0][j+1] = false; // character does not match empty string
+            } else { 
+                dp[0][j+1] = dp[0][j]; // star matches if the previous match was successful
             }
         }
 
         for (int i = 0; i < s.size(); i++) {
             for (int j = 0; j < matches.size(); j++) {
                 if (matches[j].size() == 1) { // character
+                    /* character matches if we have either the character or wildcard, 
+                    and the previous match is successful */
                     dp[i+1][j+1] = (
                         s[i] == matches[j][0] || matches[j][0] == '.'
                     ) && dp[i][j];
                 } else { // star
+                    /* star matches if ignoring the star matches (0), 
+                    or the star match extends to the current character (1+) */
                     dp[i+1][j+1] = dp[i][j+1] && (
                         matches[j][0] == s[i] || matches[j][0] == '.'
                     ) || dp[i+1][j];
                 }
             }
-        }
-
-        for (int i = 0; i < dp.size(); i++) {
-            for (int j = 0; j < dp[0].size(); j++) {
-                std::cout << dp[i][j] << ", ";
-            }
-            std::cout << "\n";
         }
 
         return dp[s.size()][matches.size()];
