7914ae83e572806519530bde9f8bf877ffd7a39f
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-10-08 13:18:20 +0100

    Time: 1 ms (4.83%), Space: 8.1 MB (17.29%) - LeetHub

---
+++ 0400-nth-digit/0400-nth-digit.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    int findNthDigit(int n) {
+        
+        // find the starting index of a given number in the integer sequence
+        auto findIndex = [&](int n) {
+
+            int num_digits = floor(log10(n)) + 1;
+
+            // find the number of digits before num_digits.
+            // pattern is 9, 90*2, 900*3, ...
+            // = 9, 9(10)(2), 9(10^2)(3), ...
+            long long result = 1;
+            for (int i = 1; i < num_digits; i++) {
+                result += 9LL * (int) pow(10, i-1) * i;
+            }
+
+            // cout << result << " ";
+
+            // add on the remaining gap
+            result += (n - (long long) pow(10, num_digits - 1)) * num_digits;
+
+            return result;
+        };
+
+        // for (int i = 1; i < 200; i++) {
+        //     cout << findIndex(i) << "\n";
+        // }
+
+        int left = 1;
+        int right = 1 << 30;
+        int mid;
+        
+        // find the smallest value with findIndex > n.
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (findIndex(mid) > (long long) n)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        cout << left;
+
+        int start_number = left - 1;
+        int remaining = n - findIndex(start_number);
+        return to_string(start_number).at(remaining) - '0';
+
+        return 0;
+    }
+};
