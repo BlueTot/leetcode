d2cb7c748354c384510d3e749ce7acdd7bec32df
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-10-13 08:45:34 +0100

    0416-PARTITION-EQUAL-SUBSET-SUM Stats: Time: 83 ms (80.96%), Space: 13.3 MB (98.04%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -10,26 +10,19 @@ public:
         if (sum % 2 == 1) return false;
         sum /= 2;
         
-        /* dp[i][j] is the number of ways to get sum j using
-        the first i elements */
-        vector<vector<bool>> dp(nums.size(), vector<bool>(sum + 1,false));
-        dp[0][0] = true;
+        /* dp[j] is the number of ways to get sum j on each iteration.
+           we continually replace the dp array to do 2d dp
+           using 1d space */
+
+        vector<bool> dp(sum + 1, false);
+        dp[0] = true;
 
         for (int i = 0; i < nums.size(); i++) {
-            for (long long j = 0; j <= sum; j++) {
-                if (i == 0) {
-                    if (j == 0)
-                        dp[i][j] = true;
-                    else
-                        dp[i][j] = nums[i] == (long long) j;
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                    if (j >= (long long) nums[i])
-                        dp[i][j] = dp[i][j] || dp[i-1][j - nums[i]];
-                }
+            for (long long j = sum; j >= nums[i]; j--) {
+                dp[j] = dp[j] || dp[j - nums[i]];
             }
         }
-        
-        return dp[nums.size()-1][sum] > 0;
+
+        return dp[sum];
     }
 };
