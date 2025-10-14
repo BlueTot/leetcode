6aa35465353c651808be485e94137f4f8f96e114
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-10-14 20:56:13 +0100

    Time: 19 ms (82.27%), Space: 17.8 MB (83.45%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+
+        /* complete knapsack approach */
+        vector<int> dp(amount + 1, 1 << 30);
+
+        dp[0] = 0;
+        for (int i = 0; i < coins.size(); i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+            }
+        }
+
+        return dp[amount] == 1 << 30 ? -1 : dp[amount];
+
+
+    }
+};
