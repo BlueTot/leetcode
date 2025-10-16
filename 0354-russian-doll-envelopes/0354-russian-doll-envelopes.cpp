344df919102b57189c66a970ccbc037b79c7cda7
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-10-16 15:54:40 +0100

    Time: 110 ms (13.63%), Space: 97.4 MB (12.8%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -11,20 +11,12 @@ public:
             return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
         };
 
-        /* two boxes fit criteria */
-        auto fits = [](const vector<int>&a, const vector<int>& b) {
-            return a[0] < b[0] && a[1] < b[1];
-        };
-
-        auto print = [](const vector<int>& a) {
-            cout << "[" << a[0] << ", " << a[1] << "]\n";
-        };
+        // /* two boxes fit criteria */
+        // auto fits = [](const vector<int>&a, const vector<int>& b) {
+        //     return a[0] < b[0] && a[1] < b[1];
+        // };
 
         sort(envelopes.begin(), envelopes.end(), compare);
-
-        for (int i = 0; i < envelopes.size(); i++) {
-            print(envelopes[i]);
-        }
         
         const int INF = 1 << 30;
         const int NEGINF = 1 << 31;
@@ -44,18 +36,15 @@ public:
                 }
             }
 
+            /* if the heights are increasing */
             if (dp[left-1][1] < envelopes[i][1] && envelopes[i][1] < dp[left][1])
                 dp[left] = envelopes[i];
 
         }
 
-        for (int i = 0; i < dp.size(); i++) {
-            print(dp[i]);
-        }
-
         int ans = 0;
         for (int l = 0; l <= envelopes.size(); l++) {
-            if (fits(dp[l], {INF, INF}))
+            if (dp[l][1] < INF)
                 ans = l;
         }
         return ans;
