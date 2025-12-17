90c7593d18d18a5f3a09c1fcc20608dfde517f97
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2025-12-17 21:37:25 +0000

    Time: 217 ms (51.96%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+    IF N <= 0 THEN
+        RETURN;
+    END IF;
+    RETURN QUERY (
+        -- Write your PostgreSQL query statement below.
+        SELECT DISTINCT e.salary FROM Employee e
+        ORDER BY e.salary DESC
+        OFFSET (N-1)
+        LIMIT 1
+    );
+END;
+$$ LANGUAGE plpgsql;
