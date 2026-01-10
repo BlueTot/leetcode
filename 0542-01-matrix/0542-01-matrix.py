360498ccfcbcb1ee1832d80a2efdc4eb7c89b5e5
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-01-10 23:15:19 +0000

    Time: 335 ms (5.08%), Space: 23.8 MB (5.7%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        
+        queue = deque()
+        n, m = len(mat), len(mat[0])
+
+        res = [[0 for _ in range(m)] for _ in range(n)]
+
+        for r in range(n):
+            for c in range(m):
+                # print(r, c, mat[r][c])
+                if mat[r][c] == 0:
+                    is_adj = False
+                    for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                        nr, nc = r+dr, c+dc
+                        if 0 <= nr < n and 0 <= nc < m and mat[nr][nc] == 1:
+                            is_adj = True
+                            break
+                    # print(r, c, is_adj)
+                    if is_adj:
+                        queue.append(((r, c), 0))
+        
+        visited = set()
+        while queue:
+            curr, dist = queue.popleft()
+            r, c = curr
+            if mat[r][c] == 1 and (r, c) not in visited:
+                res[r][c] = dist
+            visited.add(curr)
+            for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                nr, nc = r+dr, c+dc
+                if 0 <= nr < n and 0 <= nc < m and \
+                    mat[nr][nc] == 1 and (nr, nc) not in visited:
+                    queue.append(((nr, nc), dist + 1))
+            # print(queue)
+        
+        return res
+
