5f55524474384cde4e85317114c2f4150fa99166
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-01-10 11:46:08 +0000

    Time: 1307 ms (91.51%), Space: 36.7 MB (39.43%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
@@ -12,22 +12,3 @@ class Solution:
                 res = max(res, v)
         
         return res
-        
-        # @cache
-        # def dp(i: int, diff: int) -> int:
-        #     if i == 0:
-        #         return 1
-        #     res = 1
-        #     for j in range(0, i):
-        #         if nums[i] - nums[j] == diff:
-        #             res = max(res, 1 + dp(j, diff))
-        #     return res
-        
-        # res = 0
-        # for i in range(0, len(nums)):
-        #     for j in range(0, i):
-        #         res = max(res, dp(i, nums[i] - nums[j]))
-        
-        # return res
-
-
