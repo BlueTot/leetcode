ffea8ffcf6f6215725e428629b24990944edb28d
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-01-12 19:51:39 +0000

    Time: 209 ms (71.45%), Space: 0B (100%) - LeetHub

---
+++ 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    e.event_day AS day,
+    e.emp_id,
+    SUM(e.out_time - e.in_time) AS total_time
+FROM Employees e
+GROUP BY e.event_day, e.emp_id;
