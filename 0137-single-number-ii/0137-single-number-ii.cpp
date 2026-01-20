53980187eb0cdf5a2c9acce466f7e234952781a8
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-01-20 21:49:20 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        vector<int> positions(32, 0);
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            int j = 0;
+            while (num > 0) {
+                positions[j] += num & 1;
+                num >>= 1;
+                j++;
+            }
+        }
+
+        // for (int i = 0; i < positions.size(); i++) {
+        //     cout << positions[i] << "\n";
+        // }
+
+        int res = 0;
+        for (int i = positions.size()-1; i >= 0; i--) {
+            res += (positions[i] % 3);
+            if (i > 0)
+                res <<= 1;
+        }
+
+        return res;
+    }
+};
