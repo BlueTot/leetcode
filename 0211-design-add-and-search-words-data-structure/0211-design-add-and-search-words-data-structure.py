045c33202dc2e977f775c2b91e1e02a59b2c193d
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-01-22 09:57:44 +0000

    Time: 867 ms (87.61%), Space: 70.3 MB (10.66%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, key, is_final):
+        self.key = key
+        self.value = None
+        self.is_final = is_final
+        self.nexts = {}
+    
+    def __repr__(self):
+        return f"({self.key}, {self.value}, {self.is_final}, {self.nexts})"
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.__root = Node('', False)
+        
+
+    def addWord(self, word: str) -> None:
+
+        curr = self.__root
+
+        for char in word:
+
+            # if we have a child to go to
+            if char in curr.nexts:
+                curr = curr.nexts[char]
+            
+            # no child, we must create it
+            else:
+                curr.nexts[char] = Node(char, False)
+                curr = curr.nexts[char]
+        
+        # at the end, we insert the word
+        curr.is_final = True
+        curr.value = word
+
+        # print(self.__root)
+    
+    def __search(self, curr: Node, word: str, index: int) -> bool:
+
+        # print(curr.key, word, index)
+
+        if index == len(word):
+            return curr.is_final
+
+        # wildcard
+        if word[index] == '.':
+            for child in curr.nexts:
+                if self.__search(curr.nexts[child], word, index + 1):
+                    return True
+            return False
+        
+        # character
+        else:
+            if word[index] not in curr.nexts:
+                return False
+            return self.__search(curr.nexts[word[index]], word, index + 1)
+
+
+    def search(self, word: str) -> bool:
+        return self.__search(self.__root, word, 0)
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
