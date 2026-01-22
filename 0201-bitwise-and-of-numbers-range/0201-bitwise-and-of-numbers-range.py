e29d1c1ab305e64c456deed549fdcd75200a15a7
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-01-22 10:31:29 +0000

    Time: 17 ms (7.58%), Space: 19.4 MB (14.87%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -1,12 +1,6 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         
-
-        # 5 = 101
-        # 6 = 110
-        # 7 = 111
-
-        
         # next upper bound is
         # 2**(i+1)-1. if right > upper bound then we have a switch.
         # so value is 0
@@ -20,10 +14,15 @@ class Solution:
             if (left & (1 << i)) == 0:
                 continue
 
+            # we compute the largest upper bound for which
+            # the bit is the same
+            # if right exceeds this bit then the AND will be 0
+            # otherwise it will remain 1
+
             suffix = (1 << i) - 1
             next_upper = left & (~suffix) | suffix
 
-            print(i, next_upper)
+            # set the bit
             if right <= next_upper:
                 res |= (1 << i)
         
