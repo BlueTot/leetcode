5b2ee20a6f40cc2a778d8963f679a370f3fae308
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-01-25 14:13:43 +0000

    Time: 0 ms (100%), Space: 17.4 MB (22.33%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -6,9 +6,14 @@ public:
             return 0;
         }
 
+        // sort the numbers so they are in non-decreasing order
         sort(nums.begin(), nums.end());
 
         int diff = 1 << 30;
+
+        // compare differences nums[i+k-1] - nums[i] for all valid i.
+        // this is equivalent to looking at a sliding window of size k
+        
         for (int i = 0; i < nums.size() - k + 1; i++) {
             diff = min(diff, nums[i+k-1] - nums[i]);
         }
