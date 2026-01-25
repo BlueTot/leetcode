7f65cc340c4f44dd75a85a63a30c72a5b086700c
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-01-25 15:14:15 +0000

    Time: 3 ms (26.05%), Space: 19.3 MB (34.47%) - LeetHub

--- 0306-additive-number/0306-additive-number.py
+++ 0306-additive-number/0306-additive-number.py
@@ -1,6 +1,18 @@
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
 
+        # we do backtracking by selecting the next number to put into the list
+        # we keep track of the past two sums to calculate the next sum
+
+        # condition num[i] != '0' or j == i is to make sure we don't have numbers 
+        # with more than 1 digit starting with 0
+
+        # if we reach the end, then there is a match
+
+        # i: starting position.
+        # k: the number of terms
+        # num1, num2: are previous sums.
+        
         def backtrack(i, k, num1, num2):
             if i >= len(num) and k >= 3:
                 return True
