8a2f482400698de3617c1dde853cff24e318db52
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-01-25 14:37:33 +0000

    Time: 123 ms (36.92%), Space: 23 MB (23.05%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,58 +1,37 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        
-        // calculate number of differences
-        // sizes are the same
-        auto num_differences = [](string a, string b) {
-            int count = 0;
-            for (int i = 0; i < a.size(); i++) {
-                if (a[i] != b[i]) {
-                    count++;
-                }
-            }
-            return count;
-        };
 
-        // create the graph
-        unordered_map<string, vector<string>> graph;
-        for (int i = 0; i < wordList.size(); i++) {
-            for (int j = i + 1; j < wordList.size(); j++) {
-                if (num_differences(wordList[i], wordList[j]) == 1) {
-                    graph[wordList[i]].push_back(wordList[j]);
-                    graph[wordList[j]].push_back(wordList[i]);
-                }
-            }
-        }
+        // make set of words
+        unordered_set<string> words;
+        for (string word : wordList)
+            words.insert(word);
 
-        for (string word : wordList) {
-            if (num_differences(beginWord, word) == 1) {
-                graph[beginWord].push_back(word);
-                graph[word].push_back(beginWord);
-            }
-        }
-
-        // for (auto pair : graph) {
-        //     for (string value : pair.second) {
-        //         cout << pair.first << " " << value << "\n";
-        //     }
-        // }
-
-        // breadth-first-search
         unordered_set<string> visited;
         deque<pair<string, int>> queue;
         queue.push_back({beginWord, 0});
         visited.insert(beginWord);
+        string next;
+
+        // breadth-first-search
+        // time complexity is O(N * L)
 
         while (!queue.empty()) {
             auto [word, dist] = queue.front(); queue.pop_front();
             if (word == endWord) {
                 return dist + 1;
             }
-            for (string next : graph[word]) {
-                if (!visited.contains(next)) {
-                    visited.insert(next);
-                    queue.push_back({next, dist + 1});
+
+            // on each iteration, finding neighbours is O(26 * L)
+            // which is O(L)
+            for (int i = 0; i < word.size(); i++) {
+                for (char c = 'a'; c <= 'z'; c++) {
+                    next = word; // copy
+                    next = next.replace(i, 1, 1, c);
+                    if (words.contains(next) && !visited.contains(next)) {
+                        visited.insert(next);
+                        queue.push_back({next, dist + 1});
+                    }
                 }
             }
         }
