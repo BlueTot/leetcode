82eb2905d9cc55db634779bd20f32d55c5d76613
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-02-08 16:25:56 +0000

    Time: 2 ms (70.69%), Space: 19.8 MB (36.14%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,17 +1,21 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
 
+        # make list of changes in capacity
+        # (from, +passengers)
+        # (to, -passengers)
         deltas = []
         for num_passengers, fr, to in trips:
             deltas.append((fr, num_passengers))
             deltas.append((to, -num_passengers))
         
+        # sort in increasing order
         deltas.sort()
 
         curr_capacity = 0
         for _, delta in deltas:
             curr_capacity += delta
-            if curr_capacity > capacity:
+            if curr_capacity > capacity: # overflow
                 return False
         
         return True
