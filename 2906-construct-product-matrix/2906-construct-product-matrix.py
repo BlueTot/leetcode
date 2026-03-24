c46a7256d0c2b79f6aab36527a53e9b5658decc2
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-03-24 08:35:15 +0000

    Time: 215 ms (21.19%), Space: 45.8 MB (33.11%) - LeetHub

---
+++ 2906-construct-product-matrix/2906-construct-product-matrix.py
@@ -0,0 +1,41 @@
+class Solution:
+    def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        
+        n, m = len(grid), len(grid[0])
+        M = 12345
+
+        product_left = [1 for _ in range(n*m)]
+        product_right = [1 for _ in range(n*m)]
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                idx = i*m + j
+                if idx == 0:
+                    product_left[idx] = grid[i][j] % M
+                else:
+                    product_left[idx] = (product_left[idx-1] * grid[i][j]) % M
+
+        for i in range(len(grid)-1, -1, -1):
+            for j in range(len(grid[0])-1, -1, -1):
+                idx = i*m + j
+                if idx == n*m-1:
+                    product_right[idx] = grid[i][j] % M
+                else:
+                    product_right[idx] = (product_right[idx+1] * grid[i][j]) % M
+        
+        res = [[0 for _ in range(m)] for _ in range(n)]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+
+                idx = i*m + j
+                left = idx - 1
+                right = idx + 1
+
+                if idx == 0:
+                    res[i][j] = product_right[right]
+                elif idx == n*m-1:
+                    res[i][j] = product_left[left]
+                else:
+                    res[i][j] = (product_left[left] * product_right[right]) % M
+        
+        return res
