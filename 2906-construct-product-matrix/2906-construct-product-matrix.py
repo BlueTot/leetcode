13e6c2a560b1484fed2ff9d13e8e48f6546aae91
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-03-24 08:44:38 +0000

    Time: 116 ms (62.25%), Space: 44.2 MB (52.98%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.py
+++ 2906-construct-product-matrix/2906-construct-product-matrix.py
@@ -4,37 +4,20 @@ class Solution:
         n, m = len(grid), len(grid[0])
         M = 12345
 
-        product_left = [1 for _ in range(n*m)]
-        product_right = [1 for _ in range(n*m)]
+        res = [[1 for _ in range(m)] for _ in range(n)]
 
+        # left prefix sum and write to result matrix
+        curr = 1
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                idx = i*m + j
-                if idx == 0:
-                    product_left[idx] = grid[i][j] % M
-                else:
-                    product_left[idx] = (product_left[idx-1] * grid[i][j]) % M
+                res[i][j] = (res[i][j] * curr) % M
+                curr = (curr * grid[i][j]) % M
 
+        # right prefix sum and multiply with result matrix
+        curr = 1
         for i in range(len(grid)-1, -1, -1):
             for j in range(len(grid[0])-1, -1, -1):
-                idx = i*m + j
-                if idx == n*m-1:
-                    product_right[idx] = grid[i][j] % M
-                else:
-                    product_right[idx] = (product_right[idx+1] * grid[i][j]) % M
+                res[i][j] = (res[i][j] * curr) % M
+                curr = (curr * grid[i][j]) % M
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-
-                idx = i*m + j
-                left = idx - 1
-                right = idx + 1
-
-                if idx == 0:
-                    grid[i][j] = product_right[right]
-                elif idx == n*m-1:
-                    grid[i][j] = product_left[left]
-                else:
-                    grid[i][j] = (product_left[left] * product_right[right]) % M
-        
-        return grid
+        return res
