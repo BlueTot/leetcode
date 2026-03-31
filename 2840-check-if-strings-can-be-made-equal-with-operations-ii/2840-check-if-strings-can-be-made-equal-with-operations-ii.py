b6802ba02b9c3f7518471eeacfe5d8ac0a1d110a
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-03-31 13:56:39 +0100

    Time: 169 ms (27.55%), Space: 20.2 MB (60.28%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -2,19 +2,22 @@ from collections import Counter
 
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
-        
-        odd_s1, even_s1, odd_s2, even_s2 = [], [], [], []
+
+        odd_s1 = {}
+        even_s1 = {}
+        odd_s2 = {}
+        even_s2 = {}
 
         for i, c in enumerate(s1):
             if i % 2 == 0:
-                even_s1.append(c)
+                even_s1[c] = even_s1.get(c, 0) + 1
             else:
-                odd_s1.append(c)
+                odd_s1[c] = odd_s1.get(c, 0) + 1
         
         for i, c in enumerate(s2):
             if i % 2 == 0:
-                even_s2.append(c)
+                even_s2[c] = even_s2.get(c, 0) + 1
             else:
-                odd_s2.append(c)
+                odd_s2[c] = odd_s2.get(c, 0) + 1
         
-        return Counter(odd_s1) == Counter(odd_s2) and Counter(even_s1) == Counter(even_s2)
+        return odd_s1 == odd_s2 and even_s1 == even_s2
