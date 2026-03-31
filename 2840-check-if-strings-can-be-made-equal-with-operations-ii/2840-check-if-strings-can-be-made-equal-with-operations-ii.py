6be5d714cc15ff946cbe883d22ab487a6a478bf1
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-03-31 14:00:58 +0100

    Time: 177 ms (22.56%), Space: 20 MB (77.05%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -3,21 +3,23 @@ from collections import Counter
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
 
-        odd_s1 = {}
-        even_s1 = {}
-        odd_s2 = {}
-        even_s2 = {}
+        odd = {}
+        even = {}
 
         for i, c in enumerate(s1):
             if i % 2 == 0:
-                even_s1[c] = even_s1.get(c, 0) + 1
+                even[c] = even.get(c, 0) + 1
             else:
-                odd_s1[c] = odd_s1.get(c, 0) + 1
+                odd[c] = odd.get(c, 0) + 1
         
         for i, c in enumerate(s2):
             if i % 2 == 0:
-                even_s2[c] = even_s2.get(c, 0) + 1
+                if even.get(c, 0) == 0:
+                    return False
+                even[c] -= 1
             else:
-                odd_s2[c] = odd_s2.get(c, 0) + 1
+                if odd.get(c, 0) == 0:
+                    return False
+                odd[c] -= 1
         
-        return odd_s1 == odd_s2 and even_s1 == even_s2
+        return True
