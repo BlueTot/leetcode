3a814ca6cbc4e37ce7c44a4f03c23edc4af1f55c
Nok Hang Lo <69228071+BlueTot@users.noreply.github.com>
2026-04-11 11:32:07 +0100

    Time: 4 ms (47.9%), Space: 19.3 MB (76.7%) - LeetHub

---
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+
+        counter = {}
+        for i in range(len(nums)):
+            if nums[i] not in counter:
+                counter[nums[i]] = []
+            counter[nums[i]].append(i)
+        
+        res = float("inf")
+        for indices in counter.values():
+            for idx in range(max(0, len(indices) - 2)):
+                i, j, k = indices[idx], indices[idx+1], indices[idx+2]
+                res = min(res, abs(i-j) + abs(j-k) + abs(k-i))
+    
+        return -1 if res == float("inf") else res
+                
+        
